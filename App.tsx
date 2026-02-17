44c4f261d7e47e6f2d5368b129281ed5271e9594
kotkaaja <dimassakti646@gmail.com>
2026-02-18 03:30:00 +0800

    Fix casing final ke MoonScanner.tsx

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ import UserPublicProfile from './pages/UserPublicProfile';
 import Catalog          from './pages/Catalog';
 import WebhookSpammer   from './pages/WebhookSpammer';
 import UserPanel from './pages/UserPanel';
-import MoonScanner from './pages/MoonScanner';
+import MoonScanner from './pages/MoonScanner1';
 
 function Footer() {
   return (
